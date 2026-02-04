1818e980f537ae44d537b5b7bc0395fd9f6f3868
Josh Lorenzo <joshlorenzo88x@gmail.com>
2026-02-04 16:34:06 +0800

    initial commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter } from 'next/font/google';
+import { Lusitana as LusitanaType } from 'next/font/google';
+export const Lusitana = LusitanaType({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'] });
